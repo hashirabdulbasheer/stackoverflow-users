3411ce2e49e9db57b1260ba042f843c1167d7d9e
Hashir Abdulbasheer <hashir.abdulbasheer@gmail.com>
2024-01-22 22:36:13 +0300

    Added method to fetch user in abstract repository and its implementation

--- lib/features/users/data/repositories/users_repository_impl.dart
+++ lib/features/users/data/repositories/users_repository_impl.dart
@@ -1,3 +1,7 @@
+import 'package:either_dart/src/either.dart';
+import 'package:stackoverflow_users/core/models/failures.dart';
+
+import '../../domain/entities/user.dart';
 import '../../domain/repositories/users_repository.dart';
 import '../datasources/users_network_datasource.dart';
 
@@ -5,4 +9,10 @@ class SOUsersRepositoryImpl extends SOUsersRepository {
   final SOUsersNetworkDataSource networkDataSource;
 
   SOUsersRepositoryImpl({required this.networkDataSource});
+
+  @override
+  Future<Either<Failure, List<SOFUser>>> fetchUsers() {
+    // TODO: implement fetchUsers
+    throw UnimplementedError();
+  }
 }
--- lib/features/users/domain/repositories/users_repository.dart
+++ lib/features/users/domain/repositories/users_repository.dart
@@ -1,3 +1,9 @@
-import '../../../../core/base_classes/base_repository.dart';
+import 'package:either_dart/either.dart';
 
-abstract class SOUsersRepository extends BaseRepository {}
+import '../../../../core/base_classes/base_repository.dart';
+import '../../../../core/models/failures.dart';
+import '../entities/user.dart';
+
+abstract class SOUsersRepository extends BaseRepository {
+  Future<Either<Failure, List<SOFUser>>> fetchUsers();
+}
