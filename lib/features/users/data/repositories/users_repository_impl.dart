fe4600859d0e9fc9becfe99521cbf0f9a570b1ac
Hashir Abdulbasheer <hashir.abdulbasheer@gmail.com>
2024-01-22 22:59:24 +0300

    Invoking network data source from repository implementation for fetching users

--- lib/features/users/data/repositories/users_repository_impl.dart
+++ lib/features/users/data/repositories/users_repository_impl.dart
@@ -1,18 +1,30 @@
 import 'package:either_dart/either.dart';
-import 'package:stackoverflow_users/core/models/failures.dart';
 
+import '../../../../core/entities/sof_response.dart';
+import '../../../../core/models/failures.dart';
 import '../../domain/entities/user.dart';
 import '../../domain/repositories/users_repository.dart';
 import '../datasources/users_network_datasource.dart';
 
+///
+///  User repository implementation
+///
 class SOFUsersRepositoryImpl extends SOFUsersRepository {
   final SOFUsersNetworkDataSource networkDataSource;
 
   SOFUsersRepositoryImpl({required this.networkDataSource});
 
   @override
-  Future<Either<Failure, List<SOFUser>>> fetchUsers() {
-    // TODO: implement fetchUsers
-    throw UnimplementedError();
+  Future<Either<Failure, List<SOFUser>>> fetchUsers(int? page) async {
+    try {
+      SOFResponse response = await networkDataSource.fetchUsers(page: page);
+      if(response.isSuccessful) {
+        // parse response to domain model
+        return Right(List<SOFUser>.empty());
+      }
+    } catch(error) {
+      return Left(getFailure(error));
+    }
+    return Left(getDefaultFailure());
   }
 }
