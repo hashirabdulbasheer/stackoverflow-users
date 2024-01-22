ce9bab2b3cbbe331a163f6f34a190b4dd8459c91
Hashir Abdulbasheer <hashir.abdulbasheer@gmail.com>
2024-01-22 22:51:37 +0300

    Initializing easy localization for translations

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  EasyLocalization(
-      supportedLocales: const [Locale('en', 'US')],
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+  runApp(EasyLocalization(
+      supportedLocales: const [Locale('en')],
       path: 'assets/translations',
-      fallbackLocale: const Locale('en', 'US'),
-      child: const MyApp());
+      fallbackLocale: const Locale('en'),
+      child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
