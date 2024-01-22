38b35bb69ad13c9c1a57f553dea5c134978a9266
Hashir Abdulbasheer <hashir.abdulbasheer@gmail.com>
2024-01-23 00:41:20 +0300

    Implemented a sl for dependency injection using get_it

---
+++ lib/core/misc/di_container.dart
@@ -0,0 +1,25 @@
+import 'package:get_it/get_it.dart';
+import 'package:http/http.dart' as http;
+
+import '../../features/users/data/datasources/users_network_datasource.dart';
+import '../../features/users/data/repositories/users_repository_impl.dart';
+import '../../features/users/domain/repositories/users_repository.dart';
+import '../../features/users/domain/usecases/fetch_users_usecase.dart';
+
+final sl = GetIt.instance;
+
+class SOFDiContainer {
+  static Future<void> init() async {
+    /// network sources
+    sl.registerFactory<SOFUsersNetworkDataSource>(
+        () => SOFUsersNetworkDataSourceImpl(client: http.Client()));
+
+    /// repositories
+    sl.registerFactory<SOFUsersRepository>(
+        () => SOFUsersRepositoryImpl(networkDataSource: sl()));
+
+    /// use-cases
+    sl.registerFactory<SOFFetchUsersUseCase>(
+        () => SOFFetchUsersUseCase(repository: sl()));
+  }
+}
