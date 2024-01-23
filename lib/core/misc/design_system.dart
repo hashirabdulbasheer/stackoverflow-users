62257d04a505f7b189ce6dcd035855a24d8c8943
Hashir Abdulbasheer <hashir.abdulbasheer@gmail.com>
2024-01-23 08:41:46 +0300

    Added a class for design system implementation

---
+++ lib/core/misc/design_system.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class SOFDesignSystem {
+  /// sub headline test style
+  static const TextStyle subHeadline = TextStyle(
+      fontWeight: FontWeight.bold, fontSize: 20, color: Colors.black);
+}
