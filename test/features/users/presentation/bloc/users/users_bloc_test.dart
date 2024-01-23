6256b5d54097296063551935040d893b9e2209ed
Hashir Abdulbasheer <hashir.abdulbasheer@gmail.com>
2024-01-24 01:50:13 +0300

    Added user bloc testcase - Page load event loads same user if duplicate

--- test/features/users/presentation/bloc/users/users_bloc_test.dart
+++ test/features/users/presentation/bloc/users/users_bloc_test.dart
@@ -1,4 +1,5 @@
 import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
 import 'package:stackoverflow_users/core/db/hive_manager.dart';
 import 'package:stackoverflow_users/features/users/data/datasources/network/users_network_datasource.dart';
@@ -39,25 +40,42 @@ void main() {
         deleteBookmarksUseCase: deleteBookmarksUseCase);
   }
 
-  blocTest<SOFUsersListPageBloc, SOFUsersListPageState>(
-    'Initialization event loads users',
-    build: () => makeBloc(),
-    act: (bloc) {
-      bloc.add(SOFInitializeUserListPageEvent());
-    },
-    expect: () => <SOFUsersListPageState>[
-      SOFUsersListPageLoadingState(),
-      SOFUsersListPageLoadedState(users: [
-        const SOFUser(
-            id: 22656,
-            name: "JonSkeet",
-            avatar:
-                "https://www.gravatar.com/avatar/6d8ebb117e8d83d74ea95fbdd0f87e13?s=256&d=identicon&r=PG",
-            location: "Reading,UnitedKingdom",
-            age: null,
-            reputation: 1444575,
-            isBookmarked: false)
-      ], page: 1, isLoading: false),
-    ],
-  );
+  SOFUser user = const SOFUser(
+      id: 22656,
+      name: "JonSkeet",
+      avatar:
+          "https://www.gravatar.com/avatar/6d8ebb117e8d83d74ea95fbdd0f87e13?s=256&d=identicon&r=PG",
+      location: "Reading,UnitedKingdom",
+      age: null,
+      reputation: 1444575,
+      isBookmarked: false);
+
+  group("user bloc tests", () {
+    blocTest<SOFUsersListPageBloc, SOFUsersListPageState>(
+      'Initialization event loads users',
+      build: () => makeBloc(),
+      act: (bloc) {
+        bloc.add(SOFInitializeUserListPageEvent());
+      },
+      expect: () => <SOFUsersListPageState>[
+        SOFUsersListPageLoadingState(),
+        SOFUsersListPageLoadedState(users: [user], page: 1, isLoading: false),
+      ],
+    );
+
+    blocTest<SOFUsersListPageBloc, SOFUsersListPageState>(
+      'Page load event loads same user if duplicate',
+      build: () => makeBloc(),
+      act: (bloc) {
+        bloc.add(SOFInitializeUserListPageEvent());
+        bloc.add(SOFUsersListPageLoadEvent(page: 1));
+      },
+      expect: () => <SOFUsersListPageState>[
+        SOFUsersListPageLoadingState(),
+        SOFUsersListPageLoadedState(users: [user], page: 1, isLoading: false),
+        SOFUsersListPageLoadedState(users: [user], page: 1, isLoading: true),
+        SOFUsersListPageLoadedState(users: [user], page: 1, isLoading: false),
+      ],
+    );
+  });
 }
