cd749720ab15d07a45a6b156fec9ed8fb75e4052
Hashir Abdulbasheer <hashir.abdulbasheer@gmail.com>
2024-01-23 17:01:44 +0300

    Added a isBookmarked field to User model

--- lib/features/users/domain/entities/user.dart
+++ lib/features/users/domain/entities/user.dart
@@ -8,6 +8,7 @@ class SOFUser extends Equatable {
   final String location;
   final int? age;
   final int reputation;
+  final bool isBookmarked;
 
   const SOFUser(
       {required this.id,
@@ -15,24 +16,37 @@ class SOFUser extends Equatable {
       required this.avatar,
       required this.location,
       required this.reputation,
+      required this.isBookmarked,
       this.age});
 
-  SOFUser copyWith(
-      {int? id,
-      String? name,
-      String? avatar,
-      String? location,
-      int? age,
-      int? reputation}) {
+  SOFUser copyWith({
+    int? id,
+    String? name,
+    String? avatar,
+    String? location,
+    int? age,
+    int? reputation,
+    bool? isBookmarked,
+  }) {
     return SOFUser(
-        id: id ?? this.id,
-        name: name ?? this.name,
-        avatar: avatar ?? this.avatar,
-        location: location ?? this.location,
-        age: age ?? this.age,
-        reputation: reputation ?? this.reputation);
+      id: id ?? this.id,
+      name: name ?? this.name,
+      avatar: avatar ?? this.avatar,
+      location: location ?? this.location,
+      age: age ?? this.age,
+      reputation: reputation ?? this.reputation,
+      isBookmarked: isBookmarked ?? this.isBookmarked,
+    );
   }
 
   @override
-  List<Object?> get props => [id, name, avatar, location, age, reputation];
+  List<Object?> get props => [
+        id,
+        name,
+        avatar,
+        location,
+        age,
+        reputation,
+        isBookmarked,
+      ];
 }
