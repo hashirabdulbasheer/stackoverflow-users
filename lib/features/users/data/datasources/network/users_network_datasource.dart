c81faa291818541a393a4a1fa68e747b4120301b
Hashir Abdulbasheer <hashir.abdulbasheer@gmail.com>
2024-01-23 20:08:44 +0300

    Added network data source method to fetch reputation history

--- lib/features/users/data/datasources/network/users_network_datasource.dart
+++ lib/features/users/data/datasources/network/users_network_datasource.dart
@@ -9,6 +9,9 @@ import '../../../../../core/entities/sof_response.dart';
 abstract class SOFUsersNetworkDataSource extends SOFBaseNetworkDataSource {
   /// Fetch users
   Future<SOFResponse> fetchUsers({required int page});
+
+  /// Fetch reputation of user
+  Future<SOFResponse> fetchReputation({required int userId, required int page});
 }
 
 class SOFUsersNetworkDataSourceImpl implements SOFUsersNetworkDataSource {
@@ -30,6 +33,38 @@ class SOFUsersNetworkDataSourceImpl implements SOFUsersNetworkDataSource {
 
     var finalUrl = Uri.https(SOFNetworkConfig.usersBaseUrl,
         "/${SOFNetworkConfig.apiVersion}/users", queryParameters);
+
+    final headersToPass = <String, String>{};
+    headersToPass.addAll(<String, String>{
+      "Accept": "application/json;charset=utf-t",
+      "Accept-Language": "en",
+    });
+
+    http.Response response = await client.get(finalUrl, headers: headersToPass);
+
+    // Responses
+    if (response.statusCode == 201 || response.statusCode == 200) {
+      return SOFResponse(isSuccessful: true, body: response.body);
+    } else {
+      // Error
+      throw ServerException(
+          type: SOFExceptionType.network,
+          message:
+              "${SOFExceptionType.network.rawString()}: ${response.statusCode}");
+    }
+  }
+
+  @override
+  Future<SOFResponse> fetchReputation(
+      {required int userId, required int page}) async {
+    Map<String, String> queryParameters = {};
+    queryParameters[r'page'] = page.toString();
+    queryParameters[r'pagesize'] = SOFNetworkConfig.pageSize.toString();
+    queryParameters[r'userId'] = userId.toString();
+    queryParameters[r'site'] = "stackoverflow";
+
+    var finalUrl = Uri.https(SOFNetworkConfig.usersBaseUrl,
+        "/${SOFNetworkConfig.apiVersion}/reputation-history", queryParameters);
 
     final headersToPass = <String, String>{};
     headersToPass.addAll(<String, String>{
