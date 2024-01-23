d5d5f477205f810a1b429640301d02bc6c19f591
Hashir Abdulbasheer <hashir.abdulbasheer@gmail.com>
2024-01-23 07:37:15 +0300

    Added states for the users list page to hold its data

---
+++ lib/features/users/presentation/bloc/users/users_state.dart
@@ -0,0 +1,38 @@
+import 'package:equatable/equatable.dart';
+
+import '../../../../../core/models/failures.dart';
+import '../../../domain/entities/user.dart';
+
+abstract class SOFUsersListPageState {}
+
+/// Loading
+class SOFUsersListPageLoadingState extends SOFUsersListPageState {}
+
+/// Loaded
+class SOFUsersListPageLoadedState extends SOFUsersListPageState
+    with EquatableMixin {
+  final List<SOFUser> users;
+  final int page;
+
+  SOFUsersListPageLoadedState({required this.users, required this.page});
+
+  SOFUsersListPageLoadedState copyWith({
+    List<SOFUser>? users,
+    int? page,
+  }) {
+    return SOFUsersListPageLoadedState(
+        users: users ?? this.users, page: page ?? this.page);
+  }
+
+  @override
+  List<Object?> get props => [users, page];
+}
+
+/// Error
+class SOFUsersListPageErrorState extends SOFUsersListPageState {
+  final Failure failure;
+
+  SOFUsersListPageErrorState({required this.failure});
+
+  List<Object> get props => [failure];
+}
