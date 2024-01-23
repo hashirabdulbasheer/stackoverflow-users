5e1922e07a6a65e372a0c7de22e17ee535ffa0d9
Hashir Abdulbasheer <hashir.abdulbasheer@gmail.com>
2024-01-23 14:45:26 +0300

    Added a use case for fetching bookmarks from local data source

---
+++ lib/features/users/domain/usecases/fetch_bookmarks_usecase.dart
@@ -0,0 +1,18 @@
+import 'package:either_dart/either.dart';
+
+import '../../../../core/base_classes/base_usecase.dart';
+import '../../../../core/models/failures.dart';
+import '../entities/user.dart';
+import '../repositories/bookmarks_repository.dart';
+
+class SOFFetchBookmarksUseCase
+    implements SOFBaseUseCase<List<SOFUser>, NoParams> {
+  final SOFBookmarksRepository repository;
+
+  SOFFetchBookmarksUseCase({required this.repository});
+
+  @override
+  Future<Either<Failure, List<SOFUser>>> call(NoParams params) {
+    return repository.fetchBookmarks();
+  }
+}
