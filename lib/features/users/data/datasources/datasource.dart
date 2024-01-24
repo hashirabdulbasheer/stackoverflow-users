c60ec0960f12558fffe86ed4af817d3133e630d8
Hashir Abdulbasheer <hashir.abdulbasheer@gmail.com>
2024-01-24 13:21:03 +0300

    Splitted the data source into two abstract classes - local and network

--- lib/features/users/data/datasources/datasource.dart
+++ lib/features/users/data/datasources/datasource.dart
@@ -1,13 +1,20 @@
 import '../../../../core/base_classes/base_network_datasource.dart';
 import '../../../../core/entities/sof_response.dart';
 
-abstract class SOFUsersDataSource extends SOFBaseDataSource {
+abstract class SOFUsersDataSource extends SOFBaseDataSource {}
+
+abstract class SOFUsersLocalDataSource extends SOFUsersDataSource {
+  /// Fetch users
+  Future<SOFResponse> fetchUsers({required int page});
+
+  /// Save
+  Future<bool> saveUsersPage({required int page, required String responseJson});
+}
+
+abstract class SOFUsersNetworkDataSource extends SOFUsersDataSource {
   /// Fetch users
   Future<SOFResponse> fetchUsers({required int page});
 
   /// Fetch reputation of user
   Future<SOFResponse> fetchReputation({required int userId, required int page});
-
-  /// Save
-  Future<bool> saveUsersPage({required int page, required String responseJson});
 }
