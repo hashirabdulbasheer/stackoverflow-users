11f320feefe88f2653774141f980908d3a2a97b5
Hashir Abdulbasheer <hashir.abdulbasheer@gmail.com>
2024-01-24 16:52:21 +0300

    Added test case to test that Force api event should load from network and not local

--- test/features/users/presentation/bloc/users/users_bloc_test.dart
+++ test/features/users/presentation/bloc/users/users_bloc_test.dart
@@ -1,6 +1,7 @@
 import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
+import 'package:stackoverflow_users/features/users/domain/entities/user.dart';
 import 'package:stackoverflow_users/features/users/domain/usecases/delete_bookmarks_usecase.dart';
 import 'package:stackoverflow_users/features/users/domain/usecases/fetch_users_usecase.dart';
 import 'package:stackoverflow_users/features/users/domain/usecases/save_bookmarks_usecase.dart';
@@ -12,8 +13,29 @@ void main() {
   SOFUsersListPageBloc makeBloc() {
     http.Client client = TestUtils.makeUsersClient(
         response: http.Response(TestUtils.singleUserJson, 201));
+
     SOFFetchUsersUseCase fetchUsersUseCase =
         SOFFetchUsersUseCase(repository: TestUtils.makeUserRepository(client));
+
+    SOFSaveBookmarksUseCase saveBookmarksUseCase = SOFSaveBookmarksUseCase(
+        repository: TestUtils.makeBookmarksRepository());
+
+    SOFDeleteBookmarksUseCase deleteBookmarksUseCase =
+        SOFDeleteBookmarksUseCase(
+            repository: TestUtils.makeBookmarksRepository());
+
+    return SOFUsersListPageBloc(
+        fetchUsersUseCase: fetchUsersUseCase,
+        saveBookmarksUseCase: saveBookmarksUseCase,
+        deleteBookmarksUseCase: deleteBookmarksUseCase);
+  }
+
+  SOFUsersListPageBloc makeBlocWithTwoUsers() {
+    http.Client client = TestUtils.makeUsersClient(
+        response: http.Response(TestUtils.doubleUserJson, 201));
+    SOFFetchUsersUseCase fetchUsersUseCase = SOFFetchUsersUseCase(
+      repository: TestUtils.makeUserRepository(client),
+    );
     SOFSaveBookmarksUseCase saveBookmarksUseCase = SOFSaveBookmarksUseCase(
         repository: TestUtils.makeBookmarksRepository());
     SOFDeleteBookmarksUseCase deleteBookmarksUseCase =
@@ -80,6 +102,33 @@ void main() {
         SOFUsersListPageLoadedState(
             users: [TestUtils.user], page: 1, isLoading: false),
       ],
+    );
+
+    blocTest<SOFUsersListPageBloc, SOFUsersListPageState>(
+      'Force api event should load from network and not local',
+      build: () => makeBlocWithTwoUsers(),
+      act: (bloc) {
+        bloc.add(SOFInitializeUserListPageEvent());
+        bloc.add(SOFForceLoadFromApiUserListPageEvent());
+      },
+      expect: () {
+        SOFUser notBookmarked = TestUtils.user;
+        notBookmarked = notBookmarked.copyWith(isBookmarked: false);
+
+        SOFUser secondNotBookmarked = TestUtils.user;
+        secondNotBookmarked =
+            secondNotBookmarked.copyWith(isBookmarked: false, id: 33656);
+
+        return <SOFUsersListPageState>[
+          SOFUsersListPageLoadingState(),
+          SOFUsersListPageLoadedState(
+              users: [notBookmarked], page: 1, isLoading: false),
+          SOFUsersListPageLoadedState(
+              users: [secondNotBookmarked, notBookmarked],
+              page: 1,
+              isLoading: false),
+        ];
+      },
     );
   });
 }
